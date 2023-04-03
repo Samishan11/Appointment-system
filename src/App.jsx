83e1a4f9ac2f41d312b0820080204b16e4b89f4d
Samishan11 <samishanthapa0@gmail.com>
2023-04-03 17:09:54 +0545

    toast

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,19 @@ function App() {
   return (
     <Provider store={store}>
       <Routing />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      {/* Same as */}
       <ToastContainer />
     </Provider>
   )
